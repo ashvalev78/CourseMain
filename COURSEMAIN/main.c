389a68e1fdb8a597adae77f609e4bab54758ff56
Artem Kaloev <kaloev.eltech@gmail.com>
2017-05-19 01:34:18 +0300

    Unstable-1.2
    Signed-off-by: Artem Kaloev <kaloev.eltech@gmail.com>

--- COURSEMAIN/main.c
+++ COURSEMAIN/main.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct prospect {
+
+    char *name;
+    unsigned int number;
+    struct prospect *next, *prev;
+
+} prospect;
+
+typedef struct city {
+
+    unsigned int foundation, prospNumber;
+    unsigned float population;
+    char *name;
+    prospect *prosp;
+    struct city *next, *prev;
+
+} city;
+
+
+
+char **getTextFromFile() {
+
+    FILE *dataFile = NULL;
+    dataFile = fopen("/Users/artemkaloev/GitControl/CourseMain/COURSEMAIN/dataFile.txt", "r");
+
+    char **stringArray = NULL;
+
+
+
+
+
+
+}
 
 int main() {
     printf("Hello, World!\n");
@@ -9,3 +45,4 @@ int main() {
     //
     return 0;
 }
+
