b395eef729d476ca851a7dce30a224906f72ff58
ashvalev78 <ashvalev78@gmail.com>
2017-05-20 18:18:31 +0300

    Start commit

--- COURSEMAIN/main.c
+++ COURSEMAIN/main.c
@@ -1,7 +1,117 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <cstdlib>
+#include <conio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    printf("Artem Daun\n");
+typedef struct BOOK {
+    char *name;
+    int year;
+    BOOK *next;
+    BOOK *prev;
+};
+
+typedef struct AUTHOR {
+    char *name;
+    char *surname;
+    int birth;
+    int death;
+    int numbook;
+    BOOK *books;
+    AUTHOR *next;
+    AUTHOR *prev;
+};
+
+char *getstr() {
+    int i = -1;
+    char *string = NULL;
+    do {
+        i++;
+        string = (char*)realloc(string, (i + 1) * sizeof(char));
+    } while ((string[i] = getchar()) != '\n');
+    string[i] = '\0';
+    return string;
+}
+
+AUTHOR *fgetbase() {
+    FILE *base;
+    AUTHOR *Ahead, *tmp;
+    BOOK *btmp = (BOOK*)malloc(sizeof(BOOK));
+    printf("Enter the way to the author file\n ");
+    if ((base = fopen(getstr(), "r")) != NULL) {
+        tmp = Ahead = (AUTHOR*)malloc(sizeof(AUTHOR));
+        fscanf(base, "%s%s%d%d%d", Ahead->name, Ahead->surname, &Ahead->birth, &Ahead->death, &Ahead->numbook);
+        do {
+            tmp->next = (AUTHOR*)malloc(sizeof(AUTHOR));
+            (tmp->next)->prev = tmp;
+            tmp = tmp->next;
+        } while (fscanf(base, "%s%s%d%d%d", Ahead->name, Ahead->surname, &Ahead->birth, &Ahead->death, &Ahead->numbook) != EOF);
+    }
+}
+
+AUTHOR *getauth() {
+    AUTHOR *Ahead = (AUTHOR*)malloc(sizeof(AUTHOR));
+    Ahead->next = NULL;
+    Ahead->numbook = 0;
+    printf("Enter the author's name\n");
+    Ahead->name = getstr();
+    printf("Enter the author's surname\n");
+    Ahead->surname = getstr();
+    printf("Enter the author's birth and death years\n");
+    scanf("%d, %d", &Ahead->birth, &Ahead->death);
+    return Ahead;
+}
+
+BOOK *getbooks(int *num) {
+    BOOK *Bhead, *tmp;
+    tmp = Bhead = (BOOK*)malloc(sizeof(BOOK));
+    printf("Enter the name of the book\n ");
+    Bhead->name = getstr();
+    printf("Enter the date of edition\n ");
+    scanf("%d", &Bhead->year);
+    Bhead->prev = NULL;
+    *num++;
+    for (char ch = '0'; (ch = getch()) != '`';) {
+        tmp->next = (BOOK*)malloc(sizeof(BOOK));
+        (tmp->next)->prev = tmp;
+        tmp = tmp->next;
+        printf("Enter the name of the book\n ");
+        Bhead->name = getstr();
+        printf("Enter the date of edition\n ");
+        scanf("%d", &Bhead->year);
+        *num++;
+    }
+    tmp->next = NULL;
+    return Bhead;
+}
+
+AUTHOR *getauthlist () {
+    AUTHOR *Ahead, *tmp;
+    tmp = Ahead = getauth();
+    Ahead->prev = NULL;
+    Ahead->books = getbooks(&Ahead->numbook);
+    for (char ch = '0'; (ch = getch()) != '\e';) {
+        tmp->next = (AUTHOR*)malloc(sizeof(AUTHOR));
+        (tmp->next)->prev = tmp;
+        tmp = tmp->next;
+        tmp = getauth();
+        tmp->books = getbooks(&tmp->numbook);
+    }
+    return Ahead;
+}
+
+void Afree(AUTHOR *head) {
+    if (head != NULL) {
+        while (head->next != NULL) {
+            head = head->next;
+            free(head->prev);
+        }
+        free(head);
+    }
+}
+
+int main()
+{
+
+    system("pause");
     return 0;
 }
