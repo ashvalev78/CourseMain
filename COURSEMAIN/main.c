51c0287afd32478a5b1b23f4875ab6c42b5f56f0
Artem Kaloev <kaloev.eltech@gmail.com>
2017-05-21 23:20:49 +0300

    Unstable string-bases questions
    Signed-off-by: Artem Kaloev <kaloev.eltech@gmail.com>

--- COURSEMAIN/main.c
+++ COURSEMAIN/main.c
@@ -79,8 +79,8 @@ void printCityList(city *head) {
 
         for (int i = 1; link != NULL; i++, link = link->next) {
 
-            printf("Node #%d\nName - [%s]\nFoundDate - [%u]\nPopulation - [%f]\nNumberOfProspects - [%u]\n",
-                   i++, link->name, link->foundation, link->population, link->prospNumber);
+            printf("Node #%d\nName - [%s]\nFoundDate - [%u]\nPopulation - [%f]\nNumberOfProspects - [%u]\n", i,
+                   link->name, link->foundation, link->population, link->prospNumber);
 
         }
 
@@ -115,6 +115,8 @@ void printCityListReverse(city *head) {
     }
 }
 
+
+//numPosition это позиция элемента (его индекс) начала строки/числа в исходной строке "минус" один
 unsigned int getIntFromString(char *string, int *firstPosition) {
 
 
@@ -132,86 +134,130 @@ unsigned int getIntFromString(char *string, int *firstPosition) {
 
 }
 
-float getFloatFromString(char *string, int *firstPosition) {
+float getFloatFromString(char *string, int *numPosition) {
 
 
     float F1, F2, F;
     F1 = F2 = F = .0;
-    F1 = getIntFromString(string, firstPosition);
-    printf("F1 = [%f]\n", F1);
-    printf("f= [%d]\n", *firstPosition );
+    F1 = getIntFromString(string, numPosition);
 
-    int eldPos = *firstPosition;
+    /*
+     * printf("F1 = [%f]\n", F1);
+     * printf("f= [%d]\n", *numPosition);
+    */
 
-    printf("e= [%d]\n", eldPos );
+    int eldPosition = *numPosition;
 
-    F2 = getIntFromString(string, firstPosition);
-    printf("F2 = [%f]\n", F2);
+    //printf("e= [%d]\n", eldPosition );
 
-    printf("f-e = [%d]\n", *firstPosition - eldPos - 1);
-    F2 *= pow(10, -(*firstPosition - eldPos - 1));
-    printf("F2 = [%f]\n", F2);
+    F2 = getIntFromString(string, numPosition);
+    //printf("F2 = [%f]\n", F2);
+    //printf("f-e = [%d]\n", *numPosition - eldPosition - 1);
+
+
+
+    F2 *= pow(10, -(*numPosition - eldPosition - 1));
+
+    //printf("F2 = [%f]\n", F2);
 
     F = F1 + F2;
-    int precision= (*firstPosition - eldPos - 1);
-    int *pr = &precision;
-    printf("F = [%f]\n", F);
-
+    /*
+    *   int precision= (*numPosition - eldPosition - 1);
+    *   int *pr = &precision;
+    *   printf("F = [%f]\n", F);
+    */
     return F;
 
 
 }
 
-city *createNode(char *string) {
+char *getSubstringFromString(char *string, int *numPosition) {
+
+    char* substring = NULL;
+    int *j = numPosition;
+    do {
+        (*j)++;
+        substring = (char *) realloc(substring, (*j + 1) * sizeof(char));
+    } while ((substring[*j] = string[*j]) != ' '); //если строка в конце? проверка на '\0'
+    substring[*j] = '\0';
+    return substring;
+}
+
+
+prospect *createProspNode(char *string) {
+
+    prospect *link = NULL;
+    if (string[0] != '\0') {
+        link = (prospect *) malloc(sizeof(prospect));
+        int j = -1;
+        link -> name = getSubstringFromString(string, &j);
+        link -> number = getIntFromString(string, &j);
+    }
+    return link;
+
+}
+
+prospect *createProsp(char **baseString, int numString) {
+
+    prospect *head = NULL, *link = NULL;
+    head = link = (prospect *)malloc(sizeof(prospect));
+    link -> prev = NULL;
+    for (int i = 1; i < numString; i++) {
+        link->next = createProspNode(baseString[i]);
+        link->next->prev = link;
+        link = link->next;
+    }
+
+    link->next = NULL;
+
+    return head;
+}
+
+
+city *createCityNode(char *string, char **baseProspString, int numProspString) {
 
     city *link = NULL;
-    link = (city *) malloc(sizeof(city));
-    link->next = NULL;
 
     if (string[0] != '\0') {
 
-        link->name = NULL;
+        link = (city *) malloc(sizeof(city));
+
         int j = -1;
-        do {
-            j++;
-            link->name = (char *) realloc(link->name, (j + 1) * sizeof(char));
-        } while ((link->name[j] = string[j]) != ' ');
-        link -> name[j] = '\0';
+        link->name = getSubstringFromString(string, &j);
 
-
-        printf("J1 = [%d]\n", j);
+        //printf("J1 = [%d]\n", j);
         link -> foundation = getIntFromString(string, &j);
-        printf("J2 = [%d]\n", j);
-        link -> prospNumber = getIntFromString(string, &j);
-        printf("prp = [%d]\n", link -> prospNumber);
+        //printf("J2 = [%d]\n", j);
         link -> population = getFloatFromString(string, &j);
-        printf("pop = [%f]\n", link -> population);
 
-
-
+        link -> prospNumber = getIntFromString(string, &j);
+        //printf("prp = [%d]\n", link -> prospNumber);
+        //printf("pop = [%f]\n", link -> population);
+        link -> prosp = createProsp(baseProspString, numProspString);
 
     }
-
 
     return link;
 }
 
-city *create(char **baseString, int numString) {
-
-    city *headCity = NULL, *link = NULL;
+city *createCity(char **baseString, int numString, char **baseProspString, int numProspString) {
 
 
+    city *head = NULL, *link = NULL;
 
+    head = link = createCityNode(baseString[0], baseProspString, numProspString);
+
+    link->prev = NULL;
     for (int i = 1; i < numString; i++) {
-
-        //link = createNode(link, baseString[i]);
-        link = link -> next;
+        //вычленить кусок массива из общего исходного массива
+        link->next = createCityNode(baseString[i], baseProspString, numProspString);
+        link->next->prev = link;
+        link = link->next;
     }
 
+    link->next = NULL;
 
-
-
-    return headCity;
+    return head;
 }
 
 /*
@@ -279,20 +325,35 @@ char **getTextFromFile() {
 */
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Artem Commit\n");
+
 
     printf ("Enterpoint\n");
-
-    int num = 3;
     city *head = NULL;
-    head = createNode("Moscow 123 12 132.123450");
+    char **cityStringArray = NULL; int num1 = 2;
+    cityStringArray = (char **)malloc(num1 * sizeof(char*));
+    cityStringArray[0] = "Moscow 801 12.546 5";
+    cityStringArray[1] = "SPb 103 127.654 5";
+
+    char **prospStringArray = NULL;     int num2 = 5;
+    prospStringArray = (char **)malloc(num2 * sizeof(char*));
+
+    prospStringArray[0] = "Borovaya 21";
+    prospStringArray[1] = "Moscovskaya 111";
+    prospStringArray[2] = "Kulich 78";
+    prospStringArray[3] = "<!>";
+    prospStringArray[4] = "Gorod 222";
+
+
+
+
+    printf("%s\n%s\n\n", cityStringArray[0], cityStringArray[1]);
+
+    head = createCity(cityStringArray, 2);
 
     printCityList(head);
     printCityListReverse(head);
 
     printf("Endpoint\n");
-    //
     return 0;
 }
 
