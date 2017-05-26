8fc42c99b9822aaf1383205f3955084a34e9a1d3
ashvalev78 <ashvalev78@gmail.com>
2017-05-26 13:06:53 +0300

    commit 15 Adding Tests

--- main.c
+++ main.c
@@ -62,18 +62,28 @@ char **FGetABase(int *num) {
     return Amass;
 }
 
-int getnum(int *j, int i, char **str) {
+int getnum(int *j,char *str) {
     int p, num = 0;
     (*j)++;
     p = 3;
-    num = (str[i][*j] - '0') * (int)pow(10, p);
+    num = (str[*j] - '0') * (int)pow(10, p);
     (*j)++;
-    for (p = 2; str[i][*j] != ' ' && str[i][*j] != '\0'; p--, (*j)++) {
-        num = num + (str[i][*j] - '0') * (int)pow(10,p);
+    for (p = 2; str[*j] != ' ' && str[*j] != '\0'; p--, (*j)++) {
+        num = num + (str[*j] - '0') * (int)pow(10,p);
     }
     if (p >= 0)
         num = num / (int)pow(10, p + 1);
     return num;
+}
+
+char *getString (char **str, int i, int j) {
+    char *string = NULL;
+    for (int k = 0; str[i][j] != ' '; k++, j++) {
+        string = (char*)realloc(string, (k + 2) * sizeof(char));
+        string[k] = str[i][j];
+        string[k + 1] = '\0';
+    }
+    return string;
 }
 
 BOOK *BooksListFromString(char **str, int num, int *quan) { // Функция для создания списка книг из полученного списка строк. Функция для последующей работы с файлами.
@@ -97,7 +107,7 @@ BOOK *BooksListFromString(char **str, int num, int *quan) { // Функция д
         }
         //printf("%s\n", tmp->name);
         if (str[strnum][j] == ' ') {
-            tmp->year = getnum(&j, strnum, str);
+            tmp->year = getnum(&j, str[strnum]);
         }
         //printf("%d\n", tmp->year);
         head = tmp;
@@ -112,6 +122,7 @@ BOOK *BooksListFromString(char **str, int num, int *quan) { // Функция д
 
 AUTHOR *AuthListFromString(char **str, int num, char **BookMass, int Bnum) { // Функция создания списка авторов из полученного списка строк. Функция для работы с файлом.
     AUTHOR *nhead = NULL, *head = NULL, *tmp;
+    printf("NUMBER IS %d\n", num);
     for (int i = 0; i < num; i++) {
         tmp = (AUTHOR*)malloc(sizeof(AUTHOR));
         if (i == 0)
@@ -122,11 +133,14 @@ AUTHOR *AuthListFromString(char **str, int num, char **BookMass, int Bnum) { // 
         tmp->name = NULL;
         tmp->surname = NULL;
         for (int j = 0; str[i][j] != '\0'; j++) { // Функция получения имени автора, принцип понятен основан на простом считывании всех символов от начала стоки и до пробела.
+            tmp->name = getString(str, i, j);
+            /*
             for (int k = 0; str[i][j] != ' '; k++, j++) {
                 tmp->name = (char*)realloc(tmp->name, (k + 2) * sizeof(char));
                 tmp->name[k] = str[i][j];
                 tmp->name[k + 1] = '\0';
             }
+             */
             //printf("%s\n", tmp->name);
             if (str[i][j] == ' ') {
                 j++;
@@ -138,15 +152,15 @@ AUTHOR *AuthListFromString(char **str, int num, char **BookMass, int Bnum) { // 
             }
             //printf("%s\n", tmp->surname);
             if (str[i][j] == ' ') { // Функции такого рода надо будет впоследствии заменить на функцию getnum(). Собирает число из считываемых символов. Чтобы собрать число использует умножение на 10^k
-                tmp->birth = getnum( &j, i, str);
+                tmp->birth = getnum( &j, str[i]);
             }
             //printf("%d\n", tmp->birth);
             if (str[i][j] == ' ') { // Аналогично с функцией выше
-                tmp->death = getnum( &j, i, str);
+                tmp->death = getnum( &j, str[i]);
             }
             //printf("%d\n", tmp->death);
             if (str[i][j] == ' ') { // Аналогично с функцией выше
-                tmp->numbook = getnum( &j, i, str);
+                tmp->numbook = getnum( &j, str[i]);
                 /*j++;
                 p = 3;
                 tmp->numbook = (str[i][j] - '0') * ((int) pow(10, p));
@@ -237,7 +251,101 @@ AUTHOR *DeleteAFrag(AUTHOR *head, int num1, int num2) {
     return head;
 }
 
-AUTHOR *AddElement
+char **getArray(int *numString, int structure) {
+    int pos = -1, i;
+    char **stringArray = NULL;
+    printf("Enter number of elements: ");
+    *numString = getnum(&pos, getstr());
+    printf("%d\n", *numString);
+    for (i = 0; i < *numString; i++) {
+        stringArray = (char **) realloc(stringArray, (i + 1) * sizeof(char*));
+        stringArray[i] = NULL;
+        stringArray[i] = getstr();
+    }
+    if (structure == 1) { // Если это массив книг, то нужна строка-разделитель.
+        stringArray = (char**)realloc(stringArray, (i + 1) * sizeof(char*));
+        stringArray[i] = "***************************************************************************************\n";
+    }
+    return stringArray;
+}
+
+AUTHOR *GetAuth() {
+    AUTHOR *Auth;
+    int Anum = 0, Bnum = 0;
+    char **AuthArray = getArray(&Anum, 0);
+    char **BookArray = getArray(&Bnum, 1);
+    Auth = AuthListFromString(AuthArray, Anum, BookArray, Bnum);
+    return Auth;
+}
+
+BOOK *AddBookElement (BOOK *head, BOOK *element, int addNumber) {
+    if (head != NULL) {
+        BOOK *tmp = head;
+        for (int i = 1; i <= addNumber; i++) {
+            if (tmp->next == NULL) return head;
+            tmp = tmp->next;
+        }
+        if (addNumber == 0) {
+            element->next = head;
+            head->prev = element;
+            head = element;
+        } else if (tmp->next == NULL) {
+            element->prev = tmp;
+            tmp->next = element;
+        } else {
+            element->next = tmp->next;
+            element->prev = tmp;
+            tmp->next->prev = element;
+            tmp->next = element;
+        }
+    } else return element;
+    return head;
+}
+
+BOOK *AddBookFragment(BOOK *head, BOOK *Frag, int addNumber) {
+    int i = 0;
+    for (BOOK *tmp = Frag; tmp != NULL; tmp = tmp->next, i++) {
+        head = AddBookElement(head, tmp, addNumber + i);
+    }
+    return head;
+}
+
+AUTHOR *AddAuthElement(AUTHOR *head, AUTHOR *element, int addNumber) {
+    if (head != NULL) {
+        AUTHOR *tmp = head;
+        for (int i = 1; i <= addNumber; i++) {
+            if (tmp->next == NULL) return head;
+            tmp = tmp->next;
+        }
+        if (addNumber == 0) {
+            element->next = head;
+            head->prev = element;
+            head = element;
+        } else if (tmp->next == NULL) {
+            element->prev = tmp;
+            tmp->next = element;
+        } else {
+            element->next = tmp->next;
+            element->prev = tmp;
+            tmp->next->prev = element;
+            tmp->next = element;
+        }
+    } else return element;
+    return head;
+}
+
+AUTHOR *AddAuthorFragment(AUTHOR *head, AUTHOR *Frag, int addNumber) {
+    int i = 0;
+    for (AUTHOR *tmp = Frag; tmp != NULL; tmp = tmp->next, i++) {
+        head = AddAuthElement(head, tmp, addNumber + i);
+    }
+    return head;
+}
+
+AUTHOR *AddElement(AUTHOR *head, int num) {
+    //if (head == NULL) head = getAuth();
+    return head;
+}
 
 void PrintBList (BOOK *Blist) {
     if (Blist == NULL) printf("NO BOOKS!\n");
@@ -260,12 +368,14 @@ void PrintAList (AUTHOR *Alist) {
 int main()
 {
     int Anum = 0, Bnum = 0;
+    //AUTHOR *Authors = GetAuth();
+    //PrintAList(Authors);
     char **Auth = FGetABase(&Anum); // Получение массива авторов из файла
     char **Books = FGetABase(&Bnum);// Получение массива книг из файла
     AUTHOR *Ahead = AuthListFromString(Auth, Anum, Books, Bnum); // преобразование массивов авторов и книг в списки.
     PrintAList(Ahead);
     printf("******************************************************************************\n");
-    //Ahead = DeleteAFrag(Ahead, 2, 3);
+    //Ahead = DeleteAFrag(Ahead, 1, 2);
     PrintAList(Ahead);
     return 0;
 }
